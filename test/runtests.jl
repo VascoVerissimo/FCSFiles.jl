7ea0522112a23f9d850049cd82e89e7f271081ce
VascoVerissimo <vasco.verissimo@uni.lu>
2019-08-29 10:55:44 +0200

    WIP: changing function to allow larger files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
 using FCSFiles
 using FileIO
 using Test
+using Gadfly
+using Compose
+using Cairo
+using Fontconfig
 
-flowrun = load("testdata/BD-FACS-Aria-II.fcs")
+flowrun = load("testdata/file1.fcs")
 
-@test length(flowrun["SSC-A"]) == 100000
+@test length(flowrun["147Sm_CD20"]) == 1000000;
+
+p = plot(x=flowrun["154Sm_CD3"], y=flowrun["147Sm_CD20"], Geom.histogram2d, Guide.xlabel("154Sm_CD3"), Guide.ylabel("147Sm_CD20"), Coord.cartesian(xmin=0, ymin=0))
+
+draw(PNG("example.png", 10cm, 7cm, dpi=300), p)
