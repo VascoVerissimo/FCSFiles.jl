ed0d3ea33bcdba0da857a622670c4990ca7d9036
VascoVerissimo <vasco.verissimo@uni.lu>
2019-09-02 08:41:27 +0200

    calling new function metaData

--- src/FCSFiles.jl
+++ src/FCSFiles.jl
@@ -1,12 +1,16 @@
 module FCSFiles
 
 using FileIO
+using DataFrames
 
 include("type.jl")
 include("utils.jl")
 include("parse.jl")
 
-export FlowSample
+export
+    FlowSample,
+    text_mappings,
+    offsets
 
 function load(f::File{format"FCS"})
     open(f) do io
@@ -14,6 +18,8 @@ function load(f::File{format"FCS"})
 
         text_mappings = parse_text(io, offsets[1], offsets[2])
         verify_text(text_mappings)
+
+        metaData(text_mappings)
 
         return parse_data(io, offsets[3], offsets[4], text_mappings)
     end
